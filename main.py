43a581bdb6a74fce9d8487f15fdce393ccefc05f
Radek Janeček <83546285+RadekJanecek@users.noreply.github.com>
2022-02-10 11:37:37 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
 hra = False
 cheatoval1 = False
 cheatoval2 = False
+cheatoval = False
+pismeno = ""
 def main():
     global hra, cheatoval1, cheatoval2
     hra = False
@@ -16,46 +18,43 @@ def main():
 control.in_background(main)
 
 def on_forever():
-    global cheatoval1, hra, cheatoval2
+    global cheatoval1, hra, cheatoval2, cheatoval
 #    p1 = pins.digital_read_pin(DigitalPin.P1)
 #    p2 = pins.digital_read_pin(DigitalPin.P2)
     p1 = input.pin_is_pressed(TouchPin.P1)
     p2 = input.pin_is_pressed(TouchPin.P2)
-    if hra == True:
-        if p1 == True and p2 == False:
-            hra = False
-            basic.show_number(1)
-            basic.pause(3000)
-            main()
-        if p1 == False and p2 == True:
-            hra = False
-            basic.show_number(2)
-            basic.pause(3000)
-            main()
-        if p1 == True and p2 == True:
+    if cheatoval1 or cheatoval2:
+        cheatoval = True
+    if cheatoval1:
+        pismeno = "A"
+    if cheatoval1 and cheatoval2:
+        pismeno = "C"
+    if cheatoval2:
+        pismeno = "B"
+    if not hra:
+        if p1:
+            cheatoval1 = True
+        if p2:
+            cheatoval2 = True
+    if hra:
+        if p1 and p2:
             hra = False
             basic.show_string("R")
             basic.pause(3000)
-            main()
-        if cheatoval1 == True and cheatoval2 == True:
+            control.reset()
+        elif p1:
             hra = False
-            basic.show_string("C")
+            basic.show_number(1)
             basic.pause(3000)
-            main()
-        if cheatoval1 == True and cheatoval2 == False:
+            control.reset()
+        elif p2:
             hra = False
-            basic.show_string("A")
+            basic.show_number(2)
             basic.pause(3000)
-            main()
-        if cheatoval1 == False and cheatoval2 == True:
+            control.reset()
+        if cheatoval:
             hra = False
-            basic.show_string("B")
+            basic.show_string(pismeno)
             basic.pause(3000)
-            main()
-    else:
-        if p1 == True and p2 == False:
-            cheatoval1 = True
-        if p2 == True and p1 == False:
-            cheatoval2 = True
-
+            control.reset()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -3,7 +3,9 @@ pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
 let hra = false
 let cheatoval1 = false
 let cheatoval2 = false
-function main() {
+let cheatoval = false
+let pismeno = ""
+control.inBackground(function main() {
     
     hra = false
     cheatoval1 = false
@@ -18,65 +20,64 @@ function main() {
 . . # . .
 . . # . .`)
     music.playTone(Note.C, 1500)
-}
-
-control.inBackground(main)
+})
 basic.forever(function on_forever() {
+    let pismeno: string;
     
     //     p1 = pins.digital_read_pin(DigitalPin.P1)
     //     p2 = pins.digital_read_pin(DigitalPin.P2)
     let p1 = input.pinIsPressed(TouchPin.P1)
     let p2 = input.pinIsPressed(TouchPin.P2)
-    if (hra == true) {
-        if (p1 == true && p2 == false) {
-            hra = false
-            basic.showNumber(1)
-            basic.pause(3000)
-            main()
-        }
-        
-        if (p1 == false && p2 == true) {
-            hra = false
-            basic.showNumber(2)
-            basic.pause(3000)
-            main()
-        }
-        
-        if (p1 == true && p2 == true) {
-            hra = false
-            basic.showString("R")
-            basic.pause(3000)
-            main()
-        }
-        
-        if (cheatoval1 == true && cheatoval2 == true) {
-            hra = false
-            basic.showString("C")
-            basic.pause(3000)
-            main()
-        }
-        
-        if (cheatoval1 == true && cheatoval2 == false) {
-            hra = false
-            basic.showString("A")
-            basic.pause(3000)
-            main()
-        }
-        
-        if (cheatoval1 == false && cheatoval2 == true) {
-            hra = false
-            basic.showString("B")
-            basic.pause(3000)
-            main()
-        }
-        
-    } else {
-        if (p1 == true && p2 == false) {
+    if (cheatoval1 || cheatoval2) {
+        cheatoval = true
+    }
+    
+    if (cheatoval1) {
+        pismeno = "A"
+    }
+    
+    if (cheatoval1 && cheatoval2) {
+        pismeno = "C"
+    }
+    
+    if (cheatoval2) {
+        pismeno = "B"
+    }
+    
+    if (!hra) {
+        if (p1) {
             cheatoval1 = true
         }
         
-        if (p2 == true && p1 == false) {
+        if (p2) {
             cheatoval2 = true
+        }
+        
+    }
+    
+    if (hra) {
+        if (p1 && p2) {
+            hra = false
+            basic.showString("R")
+            basic.pause(3000)
+            control.reset()
+        } else if (p1) {
+            hra = false
+            basic.showNumber(1)
+            basic.pause(3000)
+            control.reset()
+        } else if (p2) {
+            hra = false
+            basic.showNumber(2)
+            basic.pause(3000)
+            control.reset()
+        }
+        
+        if (cheatoval) {
+            hra = false
+            basic.showString(pismeno)
+            basic.pause(3000)
+            control.reset()
         }
         
     }
